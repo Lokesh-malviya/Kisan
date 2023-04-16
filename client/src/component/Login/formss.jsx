941d4944025792a2bc067b7d26e6a4456a60564c
lokesh-malviya <lokeshmalviya19112002@gmail.com>
2023-04-16 19:48:59 +0530

    Access-Control-Allow-Origin 	*

--- client/src/component/Login/formss.jsx
+++ client/src/component/Login/formss.jsx
@@ -82,7 +82,8 @@ const Form = () => {
   const login = async (values, onSubmitProps) => {
     const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: { "Access-Control-Allow-Origin":"*",
+      "Content-Type": "application/json" },
       body: JSON.stringify(values),
     });
     console.log(loggedInResponse);
